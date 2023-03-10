798229e604e1613e4a1c3e18b2da6061a8ae2c65
Dan <Danevac@seznam.cz>
2023-03-10 02:24:47 +0100

    done

--- src/components/LightBox.tsx
+++ src/components/LightBox.tsx
@@ -28,7 +28,7 @@ const LightBox = ({
             onClick={() => setIsActiveLightBox(false)}
             className="flex z-[9999] items-center justify-center  absolute min-h-screen w-full md:w-screen top-0 bg-black/60 overflow-y-hidden overflow-x-hidden"
         >
-            <div className="relative flex flex-col gap-6 max-w-[315px] sm:max-w-[485px] lg:max-w-[700px] lg:max-h-[700px] ">
+            <div className="relative flex flex-col gap-6 max-w-[315px] sm:max-w-[485px] lg:max-w-[650px] ">
                 <div className=" cursor-pointer absolute -top-7 right-0 text-orange">
                     <IconClose />
                 </div>
@@ -43,13 +43,13 @@ const LightBox = ({
 
                 <div
                     onClick={(e: any) => prevSlide(e)}
-                    className="cursor-pointer bg-white flex items-center justify-center h-12 w-12 rounded-full absolute text-orange top-[37.5%] -translate-x-1/2 left-0"
+                    className="cursor-pointer bg-white flex items-center justify-center h-12 w-12 rounded-full absolute text-orange top-[50%] -translate-x-1/2 -translate-y-1/2 left-0"
                 >
                     <IconPrevious />
                 </div>
                 <div
                     onClick={(e: any) => nextSlide(e)}
-                    className="cursor-pointer bg-white flex items-center justify-center h-12 w-12 rounded-full absolute text-orange top-[37.5%] translate-x-1/2 right-0"
+                    className="cursor-pointer bg-white flex items-center justify-center h-12 w-12 rounded-full absolute text-orange top-[50%] translate-x-1/2 -translate-y-1/2 right-0"
                 >
                     <IconNext />
                 </div>
